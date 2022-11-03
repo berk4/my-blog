5995e3ce2ec3a8336b5477b9ae33ed1c17bc4214
berk4 <berkk.adali@gmail.com>
2022-11-04 00:05:09 +0300

    create logic and some components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
+import { Header } from './components/common';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+     <Header />
     </div>
   );
 }
